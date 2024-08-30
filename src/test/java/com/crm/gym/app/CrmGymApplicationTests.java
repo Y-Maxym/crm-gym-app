447e28e08464c7849e0b17f35a98652034677c55
Maxym <maxym0198@gmail.com>
2024-08-30 15:59:30 +0300

    init commit

---
+++ src/test/java/com/crm/gym/app/CrmGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.gym.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
