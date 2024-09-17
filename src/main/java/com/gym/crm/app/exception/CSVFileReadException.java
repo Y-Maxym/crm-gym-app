4402d1be486c0f70499cf7cf1de96c4a0c88bcfe
Maxym <maxym0198@gmail.com>
2024-09-17 18:25:49 +0300

    GCA-1 add liquibase migration

--- src/main/java/com/gym/crm/app/exception/CSVFileReadException.java
+++
@@ -1,8 +0,0 @@
-package com.gym.crm.app.exception;
-
-public class CSVFileReadException extends RuntimeException {
-
-    public CSVFileReadException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/gym/crm/app/exception/ParseException.java
+++
@@ -1,8 +0,0 @@
-package com.gym.crm.app.exception;
-
-public class ParseException extends RuntimeException {
-
-    public ParseException(String message) {
-        super(message);
-    }
-}
